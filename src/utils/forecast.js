1af8cfd6697123fa825c7b3de6a92a1dcc884555
Mubashir Qayyum <mubashirqayyum86@gmail.com>
2021-03-17 11:03:09 +0500

    Removed access keys :)

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const forecast = (latitude,longitude,callback) => {
 
-    const url = `http://api.weatherstack.com/current?access_key=c63c72fb41b3f658b3635383bd6c0299&query=${latitude},${longitude}`
+    const url = `http://api.weatherstack.com/current?access_key={your_access_key}&query=${latitude},${longitude}`
     console.log(url)
     request({ url, json: true }, (error, {body}) => {
                 if (error)
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const geocode = (address, callback) => {
-        const latLongApiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoibXViYXNoaXJxYXl5dW04NiIsImEiOiJja2xiMHJwazcxNTNlMnBydmRmM3d2M2V1In0.F37dAGzUHWo1mZDwcZJEbQ&limit=1'
+        const latLongApiUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token={your_access_token}&limit=1'
 
         request({ url: latLongApiUrl, json: true }, (error, response) => {
             if (error) {
